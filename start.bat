4371aec96fdfc1d8d4479a6a97cff330494e1271
ctwd-0 <suncong10@gmail.com>
2018-04-14 15:34:39 +0800

    inti

---
+++ start.bat
@@ -0,0 +1,3 @@
+cd /D "c:\DSTDS\bin"
+start dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Master
+start dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Caves
