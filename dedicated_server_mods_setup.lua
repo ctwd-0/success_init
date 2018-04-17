6b9dc51538c5aedd33ab47f9d5c2e2cd667026df
ctwd-0 <suncong10@gmail.com>
2018-04-17 11:15:51 +0800

    增加mod以及两个存档

--- dedicated_server_mods_setup.lua
+++ dedicated_server_mods_setup.lua
@@ -11,15 +11,22 @@
 	--ServerModCollectionSetup("379114180")
 ServerModSetup("347079953") --食物/药品信息显示.
 ServerModSetup("351325790") --按网格摆放.
-ServerModSetup("365119238") --智能烹饪锅.
+ServerModSetup("356930882") --无限帐篷使用.
+ServerModSetup("358749986") --强化位置指示.
+ServerModSetup("362175979") --颜色指示虫洞.
 ServerModSetup("362191380") --高ping战士保护器.
-ServerModSetup("441957490") --人物Wise Wolf.
-ServerModSetup("623749604") --可以建立大容量地窖.
+ServerModSetup("365119238") --智能烹饪锅.
 --ServerModSetup("367546858") --中文语言包.
---ServerModSetup("374550642") --改变物品堆叠上线.
---ServerModSetup("375850593") --增加一个背包和一个防具槽.
+--ServerModSetup("374550642") --改变物品堆叠上限.
+ServerModSetup("375850593") --增加一个背包和一个防具槽.
 ServerModSetup("375859599") --显示怪物血量.
-ServerModSetup("378160973") --显示血、饥饿、脑残、温度、季节、月相和天数.
-ServerModSetup("458587300") --显示玩家位置.
+ServerModSetup("376333686") --显示血、饥饿、脑残、温度、季节、月相和天数.
+ServerModSetup("378160973") --显示玩家位置.
+ServerModSetup("441957490") --人物Wise Wolf.
+ServerModSetup("458587300") --告示牌传送.
+ServerModSetup("447092740") --更多动作.
+ServerModSetup("572538624") --中文增强.
+ServerModSetup("623749604") --可以建立大容量地窖.
 ServerModSetup("666723837") --角色道士温宇.
+ServerModSetup("831523966") --999堆叠.
 ServerModSetup("1207269058") --简单的血条显示 \n附带伤害显示
--- old_games/20180415.tencent.ubuntu/.klei/DoNotStarveTogether/MyDediServer/Caves/modoverrides.lua
+++ old_games/20180415.tencent.ubuntu/.klei/DoNotStarveTogether/MyDediServer/Caves/modoverrides.lua
@@ -1,23 +1,17 @@
 return {
 	["workshop-347079953"] = { enabled = true }, --食物/药品信息显示.
-	["workshop-351325790"] = { enabled = true }, --按网格摆放.
-	["workshop-356930882"] = { enabled = true }, --无限帐篷使用.
-	["workshop-358749986"] = { enabled = true }, --强化位置指示.
-	["workshop-362175979"] = { enabled = true }, --颜色指示虫洞.
-	["workshop-362191380"] = { enabled = true }, --高ping战士保护器.
 	["workshop-365119238"] = { enabled = true }, --智能烹饪锅.
+	["workshop-351325790"] = { enabled = true }, --按网格摆放.
+	["workshop-362191380"] = { enabled = true }, --高ping战士保护器.
+	["workshop-441957490"] = { enabled = true }, --人物Wise Wolf.
+	["workshop-623749604"] = { enabled = true }, --可以建立大容量地窖.
 	--["workshop-367546858"] = { enabled = true }, --中文语言包.
-	--["workshop-374550642"] = { enabled = true }, --改变物品堆叠上限.
-	["workshop-375850593"] = { enabled = true }, --增加一个背包和一个防具槽.
+	--["workshop-374550642"] = { enabled = true }, --改变物品堆叠上线.
+	--["workshop-375850593"] = { enabled = true }, --增加一个背包和一个防具槽.
 	["workshop-375859599"] = { enabled = true }, --显示怪物血量.
 	["workshop-376333686"] = { enabled = true }, --显示血、饥饿、脑残、温度、季节、月相和天数.
 	["workshop-378160973"] = { enabled = true }, --显示玩家位置.
-	["workshop-441957490"] = { enabled = true }, --人物Wise Wolf.
 	["workshop-458587300"] = { enabled = true }, --告示牌传送.
-	["workshop-447092740"] = { enabled = true }, --更多动作.
-	["workshop-572538624"] = { enabled = true }, --中文增强.
-	["workshop-623749604"] = { enabled = true }, --可以建立大容量地窖.
 	["workshop-666723837"] = { enabled = true }, --角色道士温宇.
-	["workshop-831523966"] = { enabled = true }, --999堆叠.
 	["workshop-1207269058"] = { enabled = true }, --简单的血条显示 \n附带伤害显示
 }
