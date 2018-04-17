5a4580872396f19406c18a23cfd6f12679011487
ctwd-0 <suncong10@gmail.com>
2018-04-17 19:02:24 +0800

    4月17号下午的存档

---
+++ old_games/20180417.LK.me/Master/modoverrides.lua
@@ -0,0 +1,60 @@
+return {
+  ["workshop-1207269058"]={
+    configuration_options={
+      ddon=true,
+      hbcolor="dynamic",
+      hblength=10,
+      hbpos=1,
+      hbstyle="heart",
+      value=true 
+    },
+    enabled=true 
+  },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-356930882"]={ configuration_options={ uses=10000000, uses2=10000000 }, enabled=true },
+  ["workshop-358749986"]={
+    configuration_options={ IndicatorSize=3, MaxIndicator=7000, PlayerIndicators=1 },
+    enabled=true 
+  },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-441957490"]={ configuration_options={  }, enabled=true },
+  ["workshop-447092740"]={ configuration_options={  }, enabled=true },
+  ["workshop-458587300"]={ configuration_options={ Ownership=false, Travel_Cost=32 }, enabled=true },
+  ["workshop-572538624"]={
+    configuration_options={
+      IS_CHS_ALL_MOD=true,
+      IS_CHS_CHARACTER=true,
+      IS_CHS_FIX_ALL=true,
+      IS_CHS_SETTINGS=true 
+    },
+    enabled=true 
+  },
+  ["workshop-623749604"]={
+    configuration_options={
+      Craft="Normal",
+      Destroyable="yees",
+      FoodSpoilage=1,
+      Language="En",
+      Position="Center",
+      Slots=80 
+    },
+    enabled=true 
+  },
+  ["workshop-831523966"]={ configuration_options={  }, enabled=true } 
+}
